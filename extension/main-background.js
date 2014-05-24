775e77b248891540672e8586341ed230fcc66fb6
ahw <andrewhallagan@gmail.com>
2014-05-23 22:18:05 -0700

    Got rid of popup. Only need to click icon.
    Icon changes colors. Yay.

--- extension/main-background.js
+++ extension/main-background.js
@@ -1,36 +1,69 @@
 var listeningTabIds = {};
+var port = null; // This is the port used to communicate to the native host
 
-function stopListening(tabId, callback) {
+function changeToActiveIcon(tabId) {
+    if (typeof tabId === "undefined") {
+        // If programmer forgot to pass the tabId, return early.
+        console.warn('Must pass a tabId to changeToActiveIcon');
+        return;
+    }
+
+    chrome.browserAction.setIcon({
+        path: {
+            19: 'vim-19-pink.png',
+            38: 'vim-38-pink.png'
+        },
+        tabId: tabId
+    });
+}
+
+function changeToInactiveIcon(tabId) {
+    if (typeof tabId === "undefined") {
+        // If programmer forgot to pass the tabId, return early.
+        console.warn('Must pass a tabId to changeToInactiveIcon');
+        return;
+    }
+
+    chrome.browserAction.setIcon({
+        path: {
+            19: 'vim-19.png',
+            38: 'vim-38.png'
+        },
+        tabId: tabId
+    });
+}
+
+function stopListening(tabId) {
     delete listeningTabIds[tabId];
+    changeToInactiveIcon(tabId);
     console.log('Will stop automatically reloading tab', tabId);
-    callback('not listening');
 }
 
-function startListening(tabId, callback) {
+function startListening(tabId) {
     listeningTabIds[tabId] = tabId;
+    changeToActiveIcon(tabId);
     console.log('Will start reloading tab', tabId, 'on relevant Vim events.');
-    callback('listening');
 }
 
-// Event Page Messaging
-chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-    console.log('Event page got message from popup', request);
-    sendNativeMessage('Will ' + request.action + ' for messages from tab id ' + request.tabId);
-    if (request.action === 'toggle') {
-        if (listeningTabIds[request.tabId]) {
-            stopListening(request.tabId, sendResponse);
-        } else {
-            startListening(request.tabId, sendResponse);
-        }
-    } else if (request.action === "start") {
-        startListening(request.tabId, sendResponse);
-    } else if (request.action === 'stop') {
-        stopListening(request.tabId, sendResponse);
+chrome.browserAction.onClicked.addListener(function(tab) {
+    if (listeningTabIds[tab.id]) {
+        stopListening(tab.id);
+    } else {
+        startListening(tab.id);
+    }
+});
+
+chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
+    // This gets fired twice, first where changeInfo.status is "loading" and
+    // the second when changeInfo.status is "complete". We only need to
+    // react to the first.
+    if (changeInfo.status === 'loading' && listeningTabIds[tabId]) {
+        console.log('user refreshed a listened-for tab');
+        changeToActiveIcon(tabId);
     }
 });
 
 // Native Messaging
-var port = null;
 function sendNativeMessage(message) {
     port.postMessage(message);
 }
@@ -42,8 +75,15 @@ function onNativeMessage(eventName) {
             Object.keys(listeningTabIds).forEach(function(tabId) {
                 tabId = parseInt(tabId);
                 console.log('Found listening tab id', tabId);
-                chrome.tabs.reload(tabId, function() {
+                chrome.tabs.reload(tabId, null, function() {
                     console.log('Reloading tab id', tabId);
+                    // Check if we're listening to this tab, and change the
+                    // icon if so. We probably are still listening, but
+                    // currently manual user refreshes aren't handled.
+                    if (listeningTabIds[tabId]) {
+                        console.log('tab has reloaded; changing icon');
+                        changeToActiveIcon(tabId);
+                    }
                 });
             });
             break;
@@ -54,9 +94,7 @@ function onNativeMessage(eventName) {
 }
 
 function onDisconnected() {
-    var message = 'Failed to connect. ' + chrome.runtime.lastError.message;
-    console.log('ERROR:', message);
-    port = null;
+    console.error('Failed to connect. ' + chrome.runtime.lastError.message);
 }
 
 function connect() {
